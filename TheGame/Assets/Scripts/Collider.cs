01eec88b22dc2c15a01665a02da1bc5302309b4f
addebooi <adam@fredrikson.net>
2016-06-20 17:36:26 +0200

    Added testing collision

---
+++ TheGame/Assets/Scripts/Collider.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collider : MonoBehaviour {
+    public Rect hitBox;
+	// Use this for initialization
+	void Start () {
+
+        GameObject.FindGameObjectWithTag("ColHandler").GetComponent<CollisionHandler>().addGameObject(this.gameObject);
+        calulateSize();
+
+        //print("MINX: " + minX + " MAXX: " + maxX + " MINZ : " + minZ + " MAXZ: " + maxZ);
+        //hitBox = new Rect(new Vector2(transform.position.x, transform.position.z),
+        //                  new Vector2(maxX-minX, maxZ - minZ));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        setPosition();
+
+        
+	}
+
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawCube(new Vector3(hitBox.position.x, 0, hitBox.position.y) + (new Vector3(hitBox.size.x, 0, hitBox.size.y) / 2), new Vector3(hitBox.size.x, .2f, hitBox.size.y));
+    }
+
+    void setPosition() {
+        this.hitBox.position = transform.position;
+    }
+
+    void calulateSize() {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+
+        Vector3[] vertices = mesh.vertices;
+
+
+        float minX = float.MaxValue, maxX = -float.MaxValue, minZ = float.MaxValue, maxZ = -float.MaxValue;
+
+        Vector3 tempVal;
+        for (int i = 0; i < vertices.Length; i++) {
+            tempVal = vertices[i];
+            tempVal.x *= transform.localScale.x;
+            tempVal.z *= transform.localScale.z;
+
+            if (tempVal.x < minX)
+                minX = tempVal.x;
+            if (tempVal.x > maxX)
+                maxX = tempVal.x;
+
+            if (tempVal.z < minZ)
+                minZ = tempVal.z;
+            if (tempVal.z > maxZ)
+                maxZ = tempVal.z;
+        }
+        hitBox.size = new Vector2(maxX - minX, maxZ - minZ);
+    }
+
+    public Rect getCollider() {
+        return hitBox;
+    }
+}
---
+++ TheGame/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CollisionHandler : MonoBehaviour {
+    List<GameObject> objectsWithCollision = new List<GameObject>();
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        checkCollision();
+	}
+
+    void checkCollision() {
+        for(int i = 0; i < objectsWithCollision.Count; i++) {
+            for (int k = 1+i; k < objectsWithCollision.Count; k++) {
+
+                if (objectsWithCollision[i].GetComponent<Collider>().getCollider().Overlaps((objectsWithCollision[k].GetComponent<Collider>().getCollider()))){
+
+                    print("Colliding!");
+                }
+
+            }
+        }
+    }
+
+    public void addGameObject(GameObject obj) {
+        this.objectsWithCollision.Add(obj);
+    }
+    
+}
